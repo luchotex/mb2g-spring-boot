ea6b378e94fafa28a9e06644cf2166163bcc48e1
Luis Kupferberg <lkupferberg@overactive.com>
2019-01-20 23:35:28 -0400

    -- Added example controller.

---
+++ src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -0,0 +1,24 @@
+package guru.springframework.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Luis
+ * on 20/01/2019.
+ */
+@RestController
+public class ExampleController {
+
+    @RequestMapping("/")
+    public Map<String, Object> getIndex() {
+        Map<String, Object> returnVal = new HashMap<>();
+
+        returnVal.put("hello", "world");
+
+        return returnVal;
+    }
+}

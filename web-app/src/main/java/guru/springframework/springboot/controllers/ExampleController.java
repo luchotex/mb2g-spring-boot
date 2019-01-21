37996bbb50bbf840aebd1fc5bf1f371bb6b36200
luchotex.overactive <lkupferberg@overactive.com>
2019-01-21 11:18:29 -0400

    -- Adding the new modules to separate logic.

--- web-app/src/main/java/guru/springframework/springboot/controllers/ExampleController.java
+++ web-app/src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -1,4 +1,4 @@
-package guru.springframework.springboot.controllers;
+package springboot.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ web-model/src/main/java/guru/springframework/springboot/model/User.java
@@ -0,0 +1,4 @@
+package guru.springframework.springboot.model;
+
+public class User {
+}

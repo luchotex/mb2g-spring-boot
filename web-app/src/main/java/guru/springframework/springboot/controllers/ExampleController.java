8782a2d71d9704e21fb0ad18a1c6fef5f01254d9
luchotex.overactive <lkupferberg@overactive.com>
2019-01-21 11:19:00 -0400

    -- Added missing files.

--- web-app/src/main/java/guru/springframework/springboot/controllers/ExampleController.java
+++ web-app/src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -1,4 +1,4 @@
-package springboot.controllers;
+package guru.springframework.springboot.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
